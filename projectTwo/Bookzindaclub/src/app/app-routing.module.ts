ce7f586baea3bb906d4ba4800e2e66ff85482859
Hissongj <Hissong.joseph@gmail.com>
2019-05-14 16:28:14 -0400

    Routing to each component

--- projectTwo/Bookzindaclub/src/app/app-routing.module.ts
+++ projectTwo/Bookzindaclub/src/app/app-routing.module.ts
@@ -5,13 +5,19 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/register/registration.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { BookLookupComponent } from './components/book-lookup/book-lookup.component';
+import { ClubManagementViewComponent } from './components/club-management-view/club-management-view.component';
+import { ClubLookupComponent } from './components/club-lookup/club-lookup.component';
 
 const routes: Routes = [
   {path: '', component: LandingComponent },
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegistrationComponent},
-  {path: 'reviews', component: UserPostComponent}
+  {path: 'reviews', component: UserPostComponent},
+  {path: 'books', component: BookLookupComponent},
+  {path: 'lookup', component: ClubLookupComponent},
+  {path: 'manage', component: ClubManagementViewComponent}
 ];
 
 @NgModule({
